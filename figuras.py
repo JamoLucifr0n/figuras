d3949abc216f490044875b39edde1948fb85264d
super usuario vagrant <vagrant@vagrant.vagrant-debian-amd64>
2016-09-19 12:15:00 -0500

    inicio de protecto  , con la funcionalidad de cuarado

---
+++ figuras.py
@@ -0,0 +1,9 @@
+class Figuras:
+    
+    def cuadrado(self,lado):
+
+        try:
+            lado= int(lado)
+            return lado*lado
+        except Exception, e:
+            return 'dato incorrecto'
